ef194ee21e6805080780b2412210484e166a1b2a
AyaanMansoor <154600399+AyaanMansoor@users.noreply.github.com>
2024-03-31 23:50:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// function compareIntegers(num1, num2) {
+//     if (num1 > num2) {
+//         console.log(num1 + " is larger than " + num2);
+//     } else if (num2 > num1) {
+//         console.log(num2 + " is larger than " + num1);
+//     } else {
+//         console.log("Both integers are equal.");
+//     }
+// }
+
+
+// compareIntegers(5, 10); 
+// compareIntegers(20, 5); 
+// compareIntegers(7, 7);  
